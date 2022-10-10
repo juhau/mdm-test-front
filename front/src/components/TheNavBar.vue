21027d741dfbb5381ee7a9fb2badd29e39438bd3
yoles <yo-0ne@hotmail.fr>
2022-10-10 12:41:44 +0200

    Create navbar component and move navlink into

---
+++ front/src/components/TheNavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/cart">Panier</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style lang="scss" scoped>
+ ul {
+   display: flex;
+   list-style-type: none;
+   justify-content: center;
+ }
+ li {
+   margin: 0 15px;
+ }
+
+ a {
+   color: black;
+   &.router-link-exact-active {
+     color: #42b983;
+   }
+ }
+</style>
